e2d6cb7b348d2ea50bc263e767f8a8970c1c5c86
Unknown <sim50000@hotmail.fr>
2018-06-12 22:34:30 +0200

    Ajout .h à inclu_fich.h

--- projet/interface/inclu_fich.h
+++ projet/interface/inclu_fich.h
@@ -7,6 +7,13 @@
 #include "interf.h"
 #include "inter_1D.h"
 #include "inter_2d.h"
+#include "autocell/Rule.h"
+#include "autocell/Simulator.h"
+#include "autocell/Simulator1D.h"
+#include "autocell/Simulator2D.h"
+#include "autocell/SimulatorLifeGame.h"
+#include "autocell/SimulatorManager.h"
+#include "autocell/State.h"
 
 #include <vector>
 
