07f5425e1a25794cd504700cc37f2253a8723952
Unknown <sim50000@hotmail.fr>
2018-06-12 14:15:52 +0200

    Ajout de la transition pour 2 états

--- int/interf.cpp
+++ int/interf.cpp
@@ -1,7 +1,7 @@
 #include "inclu_fich.h"
 #include <iostream>
 
-trans::trans(QWidget* parent): QWidget(parent), nbi(0) {
+trans::trans(QWidget* parent): QWidget(parent) {
     setWindowTitle("Choix des règles");
     titre = new QVBoxLayout;
     QLabel* texte = new QLabel;
@@ -30,18 +30,33 @@ trans::trans(QWidget* parent): QWidget(parent), nbi(0) {
 
     tabrules = new QHBoxLayout;
 
+    /*for (unsigned int i = 0; i < 2; i++){
+        nbrulesvalue[i] = 0;
+        rules[i] = new QVBoxLayout;
+        rulestitle[i] = new QLabel;
+        if (!i)
+            rulestitle[i]->setText("Nombre de voisins morts");
+        else if (i == 1)
+            rulestitle[i]->setText("Nombre de voisins vivants");
+        rules[i]->addWidget(rulestitle[i]);
+        addtransition(i);
+        tabrules->addLayout(rules[i]);
+    }*/
+
+
+
     //rules = new QVector < QPair< QVBoxLayout*, QVector < QSpinBox*> > >;
     for (unsigned int i = 0; i < 2; ++i){
-        QPair<QVBoxLayout*, QVector<QSpinBox*> > *pair = new QPair<QVBoxLayout*, QVector<QSpinBox*> >;
+        QPair<QVBoxLayout*, QVector<QPair < QSpinBox*, QPushButton* > > > *pair = new QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >;
         pair->first = new QVBoxLayout;
-        pair->second = QVector<QSpinBox*>();
+        pair->second = QVector< QPair <QSpinBox*, QPushButton*> >();
         rules.push_back(pair);
         tabrules->addLayout(pair->first);
     }
     auto it = rules.begin();
     //auto it = rules.begin();
     for (unsigned int i = 0; i < 2; ++i){
-        addtransitype(*it, i);
+        addtransitype(*it);
         ++it;
     }
 
@@ -60,47 +75,77 @@ void trans::validation(){
     this->close();
 }
 
+/*void trans::addtransition(int i){
+    rulesvalue[nbrulesvalue[i]] = new QSpinBox;
+    rulesvalue[nbrulesvalue[i]]->setRange(-1,8);
+    rulesvalue[nbrulesvalue[i]]->setValue(-1);
+    rules[i]->addWidget(rulesvalue[nbrulesvalue[i]]);
+    rulesvalid[nbrulesvalue[i]] = new QPushButton("Valider");
+    rulesvalid[nbrulesvalue[i]]->setObjectName(QString::number(i));
+    rules[i]->addWidget(rulesvalid[nbrulesvalue[i]]);
+    QObject::connect(rulesvalid[nbrulesvalue[i]], SIGNAL(clicked()), this, SLOT(slot_addtransition()));
+    nbrulesvalue[i]++;
+}
 
-void trans::addtransitype(QPair< QVBoxLayout*, QVector < QSpinBox*> >* it, int i){
+void trans::slot_addtransition(){
+    QObject* senderObj = sender();
+    QPushButton* qp = qobject_cast<QPushButton*>(senderObj);
+    qp->setEnabled(false);
+    QString senderObjName = senderObj->objectName();
+    if (senderObjName == "0")
+        addtransition(0);
+    else
+        addtransition(1);
+}*/
+
+
+void trans::addtransitype(QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >* it){
     QLabel* nbneightitle = new QLabel;
     nbneightitle->setAlignment(Qt::AlignCenter);
-    if (!i)
+    /*if (!i)
         nbneightitle->setText("Nombre de voisins morts");
-    else if (i == 1)
+    else if (i == 1)*/
         nbneightitle->setText("Nombre de voisins vivants");
     //it->first->addLayout();
     it->first->addWidget(nbneightitle);
     //tabrules->addLayout(it->first); //déjà fait dans le constructeur
 
-    addnewtransi(*it, i);
+    addnewtransi(*it);
 }
 
 
-void trans::addnewtransi(QPair< QVBoxLayout*, QVector < QSpinBox*> > it, int i){
-    QSpinBox* nbpt = new QSpinBox;
-    nbpt->setRange(-1,8);
-    nbpt->setValue(-1);
+void trans::addnewtransi(QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > > it){
+    QPair < QSpinBox*, QPushButton* >* nbpt = new QPair<QSpinBox*, QPushButton*>;
+    nbpt->first = new QSpinBox;
+    nbpt->second = new QPushButton;
+    nbpt->first->setRange(-1,8);
+    nbpt->first->setValue(-1);
+    nbpt->second->setText("ajouter nouvelle règle");
     //nb.push_back(nbpt);
     //QMessageBox::warning(this, "Impossible", QString::number(nbi));
     /*auto ite = nb.begin();
     for (unsigned int j = 0; j < i; j++)
         ++ite;*/
     //(*ite).second.push_back(nbpt);
-    it.second.push_back(nbpt);
-    it.first->addWidget(nbpt);
-    add = new QPushButton("ajouter nouvelle règle");
-    it.first->addWidget(add);
+    it.second.push_back(*nbpt);
+    it.second.push_back(*nbpt);
+    it.first->addWidget(nbpt->first);
+    it.first->addWidget(nbpt->second);
 
     //add->clicked();
         //QMessageBox::warning(this, "Impossible", "Entrez une valeur valide !<br>La règle doit contenir une valeur positive ou nulle!");
-
-    QObject::connect(add, SIGNAL(clicked()), it, SLOT(addtransi()));
+    //QPair<QSpinBox*, QPushButton*> qp = it.second.last();
+    QObject::connect(it.second.last().second, SIGNAL(clicked()), this, SLOT(addtransi()));
 }
 
 
 void trans::addtransi(){
-    if (this != -1)
-        addnewtransi(pair, i);
+    QObject* send = sender();
+    QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >* sendpair = qobject_cast<QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >*>(send);
+    //QPushButton* sendpush = qobject_cast<QPushButton*>(sendpair->second.last().first);
+    QSpinBox* sendspin = qobject_cast<QSpinBox*>(sendpair->second.last().second);
+    if (sendspin->value() != -1)
+        addnewtransi(*sendpair);
     else
         QMessageBox::warning(this, "Impossible", "Entrez une valeur valide !<br>La règle doit contenir une valeur positive ou nulle!");
 }
--- int/interf.h
+++ int/interf.h
@@ -31,17 +31,25 @@ class trans : public QWidget {
 
     QPushButton* valid;
     QHBoxLayout* tabrules;
-    QVector < QPair< QVBoxLayout*, QVector < QSpinBox*> >* > rules;
-    unsigned int nbi;
+    QVector < QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >* > rules;
+    /*QVBoxLayout* rules[2];
+    QLabel* rulestitle[2];
+    QSpinBox* rulesvalue[20];
+    QPushButton* rulesvalid[20];
+    unsigned int nbrulesvalue[2];*/
 
 public:
     explicit trans(QWidget* parent = nullptr);
-    void addnewtransi(QPair< QVBoxLayout*, QVector < QSpinBox*> >, int i);
-    void addtransitype(QPair< QVBoxLayout*, QVector < QSpinBox*> >*, int i);
+    void addnewtransi(QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >);
+    void addtransitype(QPair< QVBoxLayout*, QVector < QPair< QSpinBox*, QPushButton* > > >*);
+
+    /*void addtransition(int i);*/
 
 private slots:
     void addtransi();
     void validation();
+
+    /*void slot_addtransition();*/
 
 };
 
